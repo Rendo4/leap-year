27a691e55af4d503b4abc4403a136e9368017905
Ryan Rendon <Rendon.S.Ryan@gmail.com>
2020-06-11 18:55:40 -0700

    added if statement for true or false

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    const year = parseInt($("input#year").val());
+    const result = leapYear(year);
+    $("#result").text(result);
+  })
+})
+
+function leapYear(year) {
+  if(year %4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
